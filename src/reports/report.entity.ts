6e2556098a44ade75a0c185755692f4f0e2d592c
Marc <91346941+md1440@users.noreply.github.com>
2022-03-23 17:32:59 +0100

    add report.entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Report {
@@ -7,4 +8,26 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number; // longitude
+
+  @Column()
+  lat: number; // latitude
+
+  @Column()
+  mileage: number;
+
+  // association between reports-user in db
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
