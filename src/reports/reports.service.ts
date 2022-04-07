55216f17e7feb0dec52c51484d3ffd6519e7bf8d
Marc <91346941+md1440@users.noreply.github.com>
2022-04-07 14:18:57 +0200

    bugfix in reports.service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
@@ -11,19 +11,21 @@ export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
   createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
-    return this.repo
-      .createQueryBuilder()
-      .select('AVG(price)', 'price')
-      .where('make = :make', { make })
-      .andWhere('model = :model', { model })
-      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
-      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
-      .andWhere('year - :year BETWEEN -3 AND 3', { year })
-      .andWhere('approved IS TRUE')
-      .orderBy('ABS(mileage - :mileage)', 'DESC')
-      .setParameters({ mileage })
-      .limit(3)
-      .getRawOne();
+    return (
+      this.repo
+        .createQueryBuilder()
+        .select('AVG(price)', 'price')
+        .where('make = :make', { make })
+        .andWhere('model = :model', { model })
+        .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+        .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+        .andWhere('year - :year BETWEEN -3 AND 3', { year })
+        .andWhere('approved IS TRUE')
+        // .orderBy('ABS(mileage - :mileage)', 'DESC')
+        // .setParameters({ mileage })
+        .limit(3)
+        .getRawOne()
+    );
   }
 
   create(reportDto: CreateReportDto, user: User) {
